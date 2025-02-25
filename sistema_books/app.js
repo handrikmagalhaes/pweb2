20b1712807f5c7a7de7d2ee0ea441d508b561707
Handrik Magalhaes <handrik@ifal.edu.br>
2025-02-25 10:22:27 -0300

    Script para criar SQLite

--- sistema_books/app.js
+++ sistema_books/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path')
 const basePath = path.join(__dirname);
+const sqlite3 = require('sqlite3').verbose();
 
 // Invocando o express
 const app = express()
+let db = new sqlite3.Database('petshop.db');
 
 //Ler o body como JSON
 app.use(
--- sistema_books/testes.js
+++ sistema_books/testes.js
@@ -1,4 +1,12 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Erro:', error));
+const url = "https://example.org/products.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
