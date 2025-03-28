6ef0831008bd9c99fe6a87178b801757f4b29c01
Handrik Magalhaes <handrik@ifal.edu.br>
2025-03-28 11:48:41 -0300

    Prompt

--- 02_argumentos/app.js
+++ 02_argumentos/app.js
@@ -1,4 +1,34 @@
 //Carregando o módulo minimist
 const minimist = require('minimist')
 const args = minimist(process.argv)
-console.log(args.nome)
+const operacao = args.operacao
+let resultado = 0
+switch(operacao){
+    case "soma":
+        args._.slice(2).forEach((valor) => {
+            resultado += valor
+        })
+        break;
+    case "subtracao":
+        resultado = args._.slice(2)[0]*2      
+        args._.slice(2).forEach((valor) => {
+            resultado -= valor
+        })
+        break;
+    case "multiplicacao":
+        resultado = 1
+        args._.slice(2).forEach((valor) => {
+            resultado *= valor
+        })
+        break;
+    case "divisao":
+        resultado = 1      
+        args._.slice(2).forEach((valor) => {
+            resultado /= valor
+        })
+        break;
+
+
+}
+console.log(`O resultado da operação é: ${resultado}`)
+
